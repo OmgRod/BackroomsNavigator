e1ab88872b2623a85e4ee650acd581247b963e02
OmgRod <OmgRod@users.noreply.github.com>
2025-01-04 12:26:18 +0000

    deploy: 9349e6f1e2e2823cca6a35567cfc38800c562a9e

--- utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-class Utils:
-    @staticmethod
-    def get_level_ids(csv_file):
-        return [{'label': row['id'], 'value': row['id']} for _, row in pd.read_csv(csv_file).iterrows()]
